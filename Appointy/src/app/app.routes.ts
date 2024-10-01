19a27be0d43642d64d3447c1058b85a18756f8ad
ThisIsJustAGuy <bbalint0404@gmail.com>
2024-10-01 13:23:17 +0200

    Routing start

--- Appointy/src/app/app.routes.ts
+++ Appointy/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('../home/home.module').then((mod) => mod.HomeModule),
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes, {});
