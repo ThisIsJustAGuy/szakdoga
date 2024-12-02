d2c403effe15cc0ce6d2ac219bd0ee4fe3897dd3
ThisIsJustAGuy <bbalint0404@gmail.com>
2024-12-02 08:45:45 +0100

    :bug: styles fixed

--- Appointy/src/services/style.service.ts
+++ Appointy/src/services/style.service.ts
@@ -1,30 +1,41 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StyleService {
 
-  loadStyles(){
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
+  loadStyles() {
 
     const userStylePath = 'appointy.scss';
-    const defaultStylePath = 'colors.scss';
 
     fetch(userStylePath)
       .then(response => {
-        if (response.ok) {
+        if (response.ok && !response.headers.get('content-type')?.includes('text/html')) {
+          const link = document.createElement('link');
+          link.rel = 'stylesheet';
           link.href = userStylePath;
           document.head.appendChild(link);
         } else {
-          link.href = defaultStylePath;
-          document.head.appendChild(link);
+          this.applyDefaultStyle();
         }
       })
       .catch(() => {
-        link.href = defaultStylePath;
-        document.head.appendChild(link);
+        this.applyDefaultStyle();
       });
+  }
+
+  applyDefaultStyle() {
+    const style = document.createElement('style');
+    style.textContent = `
+    :root {
+      --background-color: hsl(0 100% 100%);
+      --primary-color: hsl(215, 75%, 40%);
+      --secondary-color: hsl(215, 90%, 75%);
+
+      --secondary-text-color: white;
+    }
+  `;
+    document.head.appendChild(style);
   }
 }
